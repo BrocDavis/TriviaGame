2ec853cce4e877d8c62fadb5b3e3282fdc7e85ee
Broc <broc.davis@yahoo.com>
2019-10-06 18:42:35 -0500

    made tile for for game in html and added function to hide on call started function showQuestion to eventually show things the questions and run throught the functions.

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,22 +1,39 @@
 var questions = ["which pokemon is in the Pokedex as #1?", "What Pokeball allows you to guarantee capture or any pokemon?"];
-var questionAnswer = ["Bulbasaur","Master Ball"];
+var correctAnswer = ["Bulbasaur", "Master Ball"];
+var wrongAnswer = ["wrong"];
 var correctCount = 0;
 var wrongCount = 0;
+var gameEnd = false;
 
+showStuff();
+// setTimeout(hideStuff, 500);
+hideTitle();
 
-
-
-hideStuff();
-
-console.log(questions[0]);
-
-
-function hideStuff(){
-    $("input").hide();
-    $(".question").hide();
+function hideTitle(){
+$(".title").hide();//hide title
+}
+function hideStuff() {
+    $("input").hide();//hides radio buttons
+    $(".question").hide();//hides the question
 }
 
-function showStuff(){
+function showQuestion() {
     $("input").show();
     $(".question").show();
 }
+function showQuestions(){
+    for (var i = 0; i < questions.length; i++) {
+        if(gameEnd != false){
+            setTimeout()
+        }
+        else {
+            $(".correct-count").append(correctCount);
+            $(".wrong-count").append(wrongCount);
+        }
+    }
+}
+
+function questionTimer() {
+    showQuestions();
+}
+
